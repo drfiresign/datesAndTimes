b1472f8181148a4c2f92c84e27398f7d8bcdd022
Dylan Cascio <Dcascio73@gmail.com>
2016-09-18 21:06:28 -0700

    Updated quiz.py
    Added a bunch of methods to the quiz class. I gotta admit
    these date and times are a bit frustrating for me.

--- quiz.py
+++ quiz.py
@@ -9,8 +9,15 @@ class Quiz:
     answers = []
 
     def __init__(self):
+        question_types = (Add, Multiply)
         # generate 10 random quesitons with numbers from 1 - 10
-        # add these questions to self.questions
+        for _ in range(10):
+            # extensivle opptortunity
+            num1 = random.randint(1, 10)
+            num2 = random.randint(1, 10)
+            question = random.choice(question_types)(num1, num2)
+            # add these questions to self.questions
+            self.questions.append(question)
 
     def take_quiz(self):
         # log the start time
@@ -18,6 +25,7 @@ class Quiz:
         # log if they got the question right
         # log the end time
         # show a summary
+        pass
 
     def ask(self, question):
         # log the start time
@@ -27,7 +35,20 @@ class Quiz:
         # if the answer is right, send back True
         # otherwise send back False
         # send back the elapsed time, too
+        pass
+
+    def total_correct(self):
+        # return the total number correct
+        total = 0
+        for answer in self.answers:
+            if answer[0]:
+                total += 1
+        return total
 
     def summary(self):
         # print how many you got right & total # of questions: 5/10
+        print("You got {} out of {} right!".format(self.total_correct(), len(
+            self.questions)))
         # print the total time for the quiz: 30 seconds
+        print("It took you {} seconds total.".format((
+            self.end_time - self.start_time).seconds))
