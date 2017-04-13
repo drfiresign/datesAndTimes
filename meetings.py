1b012240ff62faea736f42abf7d5d1407958a9bb
Dylan Cascio <dcascio73@gmail.com>
2017-04-13 01:41:58 -0700

    Added file missing from earlier work

--- meetings.py
+++ meetings.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import pytz
 
 OTHER_TIMEZONES = [
-    pytz.timezone('US/Eastern'), pytz.timezone('Pacific/Aukland'),
+    pytz.timezone('US/Eastern'), pytz.timezone('Pacific/Auckland'),
     pytz.timezone('Asia/Calcutta'), pytz.timezone('UTC'),
     pytz.timezone('Europe/Paris'), pytz.timezone('Africa/Khartoum')
 ]
